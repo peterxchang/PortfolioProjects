280ffbf3fef004f6dc7c6e82ffaa6e8ec3189dd3
Peter Chang <80991027+peterxchang@users.noreply.github.com>
2022-08-10 21:28:38 -0700

    Update SQL Queries for Tableau.sql

--- SQL Queries for Tableau.sql
+++ SQL Queries for Tableau.sql
@@ -9,7 +9,7 @@ Queries used for Tableau COVID-19 Project
 -- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From [Portfolio Project]..CovidDeaths
 --Where location like '%states%'
 where continent is not null 
 --Group By date
@@ -28,7 +28,7 @@ order by 1,2
 
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From [Portfolio Project]..CovidDeaths
 --Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
@@ -39,7 +39,7 @@ order by TotalDeathCount desc
 -- 3.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From [Portfolio Project]..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
@@ -49,7 +49,7 @@ order by PercentPopulationInfected desc
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From [Portfolio Project]..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
